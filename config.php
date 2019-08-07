76e72cf74d7728e5adac2a70b7c7ca0b33bf19b6
Илья Корон <46747598+ELCORONE@users.noreply.github.com>
2019-08-07 17:08:43 +1000

    Файл конфигурации

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// Используемая версия API
+define('VK_API_VERSION', '5.67');
+// Ссылка на API
+define('VK_API_METHOD', "https://api.vk.com/method/");
+// Определение API_TOKEN
+define('API_TOKEN', ""); // Вставить API TOKEN
+
+$confirmation_token = '';// Вставить ответное сообщение
+$token = "";             // Вставить API TOKEN
+$secretkey = '';
+
+$sql_host = 'localhost';
+$sql_base = 'db';
+$sql_user = 'root';
+$sql_pass = '';
+$sql_tabl = 'table';
+?>
